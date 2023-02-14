cc1c4bb716ee6d15e812775ecbd6617f6b44db44
Philipp <philipp.zimmermann@bevelop.de>
2023-02-14 19:48:16 +0100

    default heuristic

---
+++ project1/python_src/DefaultHeuristic.py
@@ -0,0 +1,36 @@
+
+BLACK, WHITE, EMPTY = "B", "W", " "
+
+
+class DefaultHeuristics:
+    """
+    Try to move forward in the field.
+    Do not consider captures at all.
+    """
+
+    @staticmethod
+    def eval(state, role):
+        board_height = len(state.board)
+        distance_black = board_height
+        distance_white = board_height
+
+        for y in range(len(state.board)):
+            for x in range(len(state.board[0])):
+                if state.board[y][x] == BLACK:
+                    distance_black = min(y, distance_black)
+                elif state.board[y][x] == WHITE:
+                    distance_white = min(board_height-y-1, distance_white)
+
+        if role == "white":
+            if distance_white == 0:
+                return 100
+            if distance_black == 0:
+                return -100
+            return distance_black-distance_white
+        else:
+            if distance_black == 0:
+                return 100
+            if distance_white == 0:
+                return -100
+            return distance_white-distance_black
+
