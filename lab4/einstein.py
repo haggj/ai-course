9c09b46a3e8e2b0e1c9408de7358d22592b4d59c
Philipp <philipp.zimmermann@bevelop.de>
2023-02-20 02:04:54 +0100

    solution

--- lab4/einstein.py
+++ lab4/einstein.py
@@ -6,7 +6,7 @@ from ortools.sat.python import cp_model
 # this function needs to create the csp model and return the model and a list of variables in the model
 def setup_csp():
     model = cp_model.CpModel()
-    variables = []
+    variables = {}
     colors = ["Red", "Green", "Ivory", "Yellow", "Blue"]
     nations = ["Englishman", "Spaniard", "Norwegian", "Ukrainian", "Japanese"]
     cigarettes = ["Old Gold", "Kools", "Chesterfields", "Lucky Strike", "Parliaments"]
@@ -19,7 +19,9 @@ def setup_csp():
     # variables.append(v1)
     # v2 =  model.NewIntVar(1, 5, "variable2")
     # variables.append(v2)
-    
+
+    for var in colors + nations + cigarettes + drinks + pets:
+        variables[var] = model.NewIntVar(1, 5, var)
 
     # TODO: 2. Add the constraints to the model!
     # You might need model.Add(), model.addAbsEquality() and model.AddAllDifferent()
@@ -30,23 +32,44 @@ def setup_csp():
     # model.Add(v1 == v2 + 2)
     # model.addAbsEquality(2, v1 - v2) # meaning that abs(v1-v2) == 2
     # etc.
-        
-    # The Englishman lives in the red house.
-    # The Spaniard owns the dog.
-    # Coffee is drunk in the green house.
-    # The Ukrainian drinks tea.
-    # The green house is immediately to the right of the ivory house.
-    # The Old Gold smoker owns snails.
-    # Kools are smoked in the yellow house.
-    # Milk is drunk in the middle house.
-    # The Norwegian lives in the first house.
-    # The man who smokes Chesterfields lives in the house next to the man with the fox.
-    # Kools are smoked in the house next to the house where the horse is kept.
-    # The Lucky Strike smoker drinks orange juice.
-    # The Japanese smokes Parliaments.
-    # The Norwegian lives next to the blue house.
-    # Each of the five houses has a different color, each of the five inhabitants has a different nationality, prefers a different brand of cigarettes, a different drink, and owns a different pet.
 
+    # The Englishman lives in the red house.
+    model.Add(variables["Englishman"] == variables["Red"])
+    # The Spaniard owns the dog.
+    model.Add(variables["Spaniard"] == variables["Dog"])
+    # Coffee is drunk in the green house.
+    model.Add(variables["Coffee"] == variables["Green"])
+    # The Ukrainian drinks tea.
+    model.Add(variables["Ukrainian"] == variables["Tea"])
+    # The green house is immediately to the right of the ivory house.
+    model.Add((variables["Green"] + 1) == variables["Ivory"])
+    # The Old Gold smoker owns snails.
+    model.Add(variables["Old Gold"] == variables["Snails"])
+    # Kools are smoked in the yellow house.
+    model.Add(variables["Kools"] == variables["Yellow"])
+    # Milk is drunk in the middle house.
+    model.Add(variables["Milk"] == 3)
+    # The Norwegian lives in the first house.
+    model.Add(variables["Norwegian"] == 1)
+    # The man who smokes Chesterfields lives in the house next to the man with the fox.
+    model.AddAbsEquality(1, (variables["Chesterfields"] - variables["Fox"]))
+    # Kools are smoked in the house next to the house where the horse is kept.
+    model.AddAbsEquality(1, (variables["Kools"] - variables["Horse"]))
+    # The Lucky Strike smoker drinks orange juice.
+    model.Add(variables["Lucky Strike"] == variables["Orange juice"])
+    # The Japanese smokes Parliaments.
+    model.Add(variables["Japanese"] == variables["Parliaments"])
+    # The Norwegian lives next to the blue house.
+    model.AddAbsEquality(1, (variables["Norwegian"] - variables["Blue"]))
+    # Each of the five houses has a different color, each of the five inhabitants has a different nationality, prefers a different brand of cigarettes, a different drink, and owns a different pet.
+    model.AddAllDifferent(variables["Red"], variables["Green"], variables["Ivory"], variables["Yellow"], variables["Blue"])
+    model.AddAllDifferent(variables["Englishman"], variables["Spaniard"], variables["Norwegian"], variables["Ukrainian"], variables["Japanese"])
+    model.AddAllDifferent(variables["Old Gold"], variables["Kools"], variables["Chesterfields"], variables["Lucky Strike"], variables["Parliaments"])
+    model.AddAllDifferent(variables["Water"], variables["Orange juice"], variables["Tea"], variables["Coffee"], variables["Milk"])
+    model.AddAllDifferent(variables["Zebra"], variables["Dog"], variables["Fox"], variables["Snails"], variables["Horse"])
+
+    # OR Tools (VarArraySolutionPrinter) can not handel Dictionaries
+    variables = list(variables.values())
     return model, variables
 
 
