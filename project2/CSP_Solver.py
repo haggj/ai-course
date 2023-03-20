45cc94b148a370dfe05019fe4e69d1af3315b86e
Philipp <philipp.zimmermann@bevelop.de>
2023-03-21 00:36:23 +0100

    [ADD] count possible solutions

--- project2/CSP_Solver.py
+++ project2/CSP_Solver.py
@@ -1,16 +1,39 @@
 from sudoku import SudokuBoard
 from ortools.sat.python import cp_model
 
+import time
+
 EMPTY_SPACE = 0
+
+# CSP Callback function executed on each found Solution
+class VarArraySolutionCounter(cp_model.CpSolverSolutionCallback):
+    """Count intermediate solutions."""
+
+    def __init__(self):
+        cp_model.CpSolverSolutionCallback.__init__(self)
+        self.__solution_count = 0
+
+    def on_solution_callback(self):
+        self.__solution_count += 1
+        #print("Solution #", str(self.__solution_count))
+
+    def solution_count(self):
+        return self.__solution_count
 
 class CSP_Solver:
     def __init__(self, board=SudokuBoard(3)):
+        # initialize Board
         self.board = board._board
         self.board_size = board.size
         self.board_size_sqrt = board.size_sqrt
-        self.model = cp_model.CpModel()
         self.variables = []
         self.solver_variables = []
+        # csp env
+        self.model = cp_model.CpModel()
+        self.solution_counter = VarArraySolutionCounter()
+
+    # Create CSP Variables
+    # Setup all Constraints depending on Board & Sudoku Rules
     def setup_csp(self):
         # Create all variables
         for i_x, row in enumerate(self.board):
@@ -55,35 +78,67 @@ class CSP_Solver:
             for space in row:
                 self.solver_variables.append(space)
 
+    # Solve Sudoku
+    # Save first found Solution in self.board
     def solve_csp(self):
         # create the model
         self.setup_csp()
         # create the solver
         solver = cp_model.CpSolver()
+        # find first solution
         status = solver.Solve(self.model)
-        #solution_printer = cp_model.VarArraySolutionPrinter(self.solverVariables)
-        # find all solutions and print them out
-        #status = solver.SearchForAllSolutions(self.model, solution_printer)
         if status == cp_model.INFEASIBLE:
             print("ERROR: Model does not have a solution!")
-        elif status == cp_model.MODEL_INVALID:
-            print("ERROR: Model is invalid!")
-            self.model.Validate()
-        elif status == cp_model.UNKNOWN:
-            print("ERROR: No solution was found!")
-        else:
-            #n = solution_printer.solution_count()
-            #print("%d solution(s) found." % n)
+        elif status == cp_model.FEASIBLE:
+            print("Solution is FEASIBLE")
+        elif status == cp_model.OPTIMAL:
+            print("Solution is OPTIMAL")
             # Create Sudoku Board from Solved Result
             for x in range(0, self.board_size):
                 for y in range(0, self.board_size):
                     self.board[x][y] = solver.Value(self.variables[x][y])
-            #print(self.board)
+        else:
+            print("ERROR: Solution Status unexpected!")
 
-board = SudokuBoard(3)
+    # return number of unique & valid solutions
+    # return -1 if no solution
+    def get_num_solutions(self, board=SudokuBoard(3)):
+        # initialize Board
+        self.board = board
+        self.board = board._board
+        self.board_size = board.size
+        self.board_size_sqrt = board.size_sqrt
+        # create the model
+        self.setup_csp()
+        # create the solver
+        solver = cp_model.CpSolver()
+        # find all solutions
+        status = solver.SearchForAllSolutions(self.model, self.solution_counter)
+        if status == cp_model.INFEASIBLE:
+            print("ERROR: Model does not have a solution!")
+            return -1
+        elif status == cp_model.FEASIBLE:
+            print("Solution is FEASIBLE")
+            n = self.solution_counter.solution_count()
+            print("%d solution(s) found." % n)
+            return n
+        elif status == cp_model.OPTIMAL:
+            print("Solution is OPTIMAL")
+            n = self.solution_counter.solution_count()
+            print("%d solution(s) found." % n)
+            return n
+        else:
+            print("ERROR: Solution Status unexpected!")
+            return -1
+
+# measure Time
+start = time.time()
+board = SudokuBoard(5)
 solver = CSP_Solver(board)
-#solver.setup_csp()
 solver.solve_csp()
+#solver.get_num_solutions(board)
 
-board.set_board(solver.board)
-print(board)
+#board.set_board(solver.board)
+#print(board)
+end = time.time()
+print("CSP took: ", end - start, "seconds")
