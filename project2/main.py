6539e2f62a8617e5c7f281c370e5535e8cffe396
Jonathan Lutz <jonathan.lutz@freenet.de>
2023-04-04 11:45:51 +0000

    Start statistics from main.py

--- project2/main.py
+++ project2/main.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 from CSP_Solver import CSP_Solver
 from dfs import SudokuDFS
 from sudoku import Version
+from statistics import Statistics
 
 
 def info(msg, pre=""):
@@ -28,15 +29,33 @@ def run(ALG):
         info(f"Version 'rec.store_legal' starting", pre="\n\n")
         dfs.solve_recursive(sudoku, Version.STORE_LEGAL)
 
-
+def plot(args):
+    statistics = Statistics()
+    if isinstance(args.n, int):
+        info(f"Input more than one sudoku size to plot, e.g. --n 2 3 4", pre="\n\n")
+        return
+    if args.plot_comparison:
+        info(f"Compare strategies starting for sudoku sizes {args.n}", pre="\n\n")
+        statistics.compare_strategies(args.n, args.s)
+    if args.plot_numbers_removed:
+        info(f"Get percentage of removed numbers starting for sudoku sizes {args.n}", pre="\n\n")
+        statistics.get_perc_numbers_removed(args.n, args.s)
 
 if __name__=="__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
     algorithms = ["all", "sorted", "store_legal", "rec.sorted", "rec.store_legal"]
     parser.add_argument('--algorithm', type=str, default="all", help="The algorithms which you want to run to solve the Sudoku.", choices=algorithms)
-    parser.add_argument('--n', type=int, default=3, help="This parameter controls the size of the generated board. E.g. if size is 3, the generated board is 9x9.")
+    parser.add_argument('--n', type=int, nargs="+", default=3, help="This parameter controls the size of the generated board. E.g. if size is 3, the generated board is 9x9.")
+    parser.add_argument('--plot_comparison', type=bool, default=False, help="With this parameter the required time of different DFS strategies and the CSP solver can be plotted.")
+    parser.add_argument('--plot_numbers_removed', type=bool, default=False, help="With this parameter the percentage of numbers removed until the sudoku no longer has only one solution can be plotted.")
+    parser.add_argument('--s', type=int, default=10, help="This parameter controls the number of experiments which are run to plot the statistics.")
     args = parser.parse_args()
+
+    # Plot statistics
+    if args.plot_comparison or args.plot_numbers_removed:
+        plot(args)
+        exit()
 
     # Generate sudoku via CSP solver
     size = args.n * args.n
--- project2/statistics.py
+++ project2/statistics.py
@@ -15,6 +15,8 @@ class Statistics:
     def get_perc_numbers_removed(self, sudoku_size, n):
         perc_removed_numbers = []
         std_removed_numbers = []
+
+        # Run n experiments for different sudoku sizes and record statistics
         for size in sudoku_size:
             avg_removed_numbers = 0
             removed_numbers = []
@@ -26,8 +28,8 @@ class Statistics:
             perc_removed_numbers.append(avg_removed_numbers/(size**4)*100)
             std_removed_numbers.append(np.std(removed_numbers))
 
-        print("Average percentage removed numbers: ", perc_removed_numbers)
-        print("Standard deviation: ", std_removed_numbers)
+        # Plot results
+        print("Percentage of removed numbers: ", perc_removed_numbers)
         plt.figure()
         plt.plot(sudoku_size, perc_removed_numbers)
         plt.xlabel('Sudoku Size')
@@ -43,82 +45,54 @@ class Statistics:
         plt.show()
         return
     
-    def compare_CSP_strategies(self, sudoku_size, n):
-        start = time.time()
-        avg_time = [0] * 4
-        std_time = [0] * 4
-        strategies = [None, cp_model.CHOOSE_FIRST, cp_model.CHOOSE_MIN_DOMAIN_SIZE, cp_model.CHOOSE_MAX_DOMAIN_SIZE]
-        strategies_str = ['DEFAULT', 'CHOOSE_FIRST', 'CHOOSE_MIN_DOMAIN_SIZE', 'CHOOSE_MAX_DOMAIN_SIZE']
-        strategies_int = [0, 1, 2, 3]
-        solver = CSP_Solver()
-        unique_sudokus = [solver.generate_random_sudoku(sudoku_size, None, remove_numbers=int(0.5*(sudoku_size**4))) for _ in range(n)]
-        for i, strategy in enumerate(strategies):
-            times = []
-            for j in range(n):
+    def compare_strategies(self, sudoku_size, n):
+        avg_time = []
+        std_time = []
+
+        # Run n experiments for different sudoku sizes and record statistics
+        for size in sudoku_size:
+            times = [[] for i in range(5)]
+            for i in range(n):
+                solver = CSP_Solver()
+                sudoku = solver.generate_unique_sudoku(size, 40)
                 start = time.time()
-                solver.solve_csp(unique_sudokus[j], decision_strategy=strategy)
+                solver.solve_csp(sudoku, decision_strategy=None)
                 end = time.time()
-                times.append(end - start)
-            avg_time[i] = np.mean(times)
-            std_time[i] = np.std(times)
+                times[0].append(end-start)
+                dfs = SudokuDFS(board=sudoku)
+                sol1 = dfs.solve(Version.SORTED)
+                times[4].append(dfs.duration)
+                sol2 = dfs.solve(Version.STORE_LEGAL)
+                times[2].append(dfs.duration)
+                sol3 = dfs.solve_recursive(sudoku, Version.SORTED)
+                times[3].append(dfs.duration)
+                sol4 = dfs.solve_recursive(sudoku, Version.STORE_LEGAL)
+                times[1].append(dfs.duration)
+            avg_time.append([np.mean(t) for t in times])
+            std_time.append([np.std(t) for t in times])
+        
+        # Plot results
         print("Average time: ", avg_time)
-        print("Standard deviation: ", std_time)
         plt.figure()
-        plt.plot(strategies_int, avg_time)
-        plt.xlabel('Decision Strategy')
-        plt.xticks(strategies_int, strategies_str)
-        plt.ylabel('Average time')
-        plt.title('Average time for different decision strategies')
-        plt.figure()
-        plt.plot(strategies_int, std_time)
-        plt.xlabel('Decision Strategy')
-        plt.xticks(strategies_int, strategies_str)
-        plt.ylabel('Standard deviation')
-        plt.title('Standard deviation of time for different decision strategies')
-        plt.show()
-        return
-    
-    def compare_DFS_strategies(self, sudoku_size, n):
-        times = [[] for i in range(5)]
-        for i in range(n):
-            solver = CSP_Solver()
-            sudoku = solver.generate_unique_sudoku(sudoku_size)
-            start = time.time()
-            solver.solve_csp(sudoku, decision_strategy=None)
-            end = time.time()
-            times[0].append(end-start)
-            dfs = SudokuDFS(board=sudoku)
-            sol1 = dfs.solve(Version.SORTED)
-            times[4].append(dfs.duration)
-            sol2 = dfs.solve(Version.STORE_LEGAL)
-            times[2].append(dfs.duration)
-            sol3 = dfs.solve_recursive(sudoku, Version.SORTED)
-            times[3].append(dfs.duration)
-            sol4 = dfs.solve_recursive(sudoku, Version.STORE_LEGAL)
-            times[1].append(dfs.duration)
-        # print("Times: ", times)
-        avg_time = [np.mean(t) for t in times]
-        std_time = [np.std(t) for t in times]
-        print("Average time: ", avg_time)
-        print("Standard deviation: ", std_time)
-        plt.figure()
-        plt.plot([0, 1, 2, 3, 4], avg_time)
+        for size in range(len(sudoku_size)):
+            plt.plot([0, 1, 2, 3, 4], avg_time[size], label=str(sudoku_size[size]**2) + 'x' + str(sudoku_size[size]**2))
         plt.xlabel('Decision Strategy')
         plt.xticks([0, 1, 2, 3, 4], ['CSP', 'DFS_STORE_LEGAL_RECURSIVE', 'DFS_STORE_LEGAL', 'DFS_SORTED_RECURSIVE', 'DFS_SORTED'])
         plt.ylabel('Average time [s]')
         plt.title('Average time for different decision strategies')
+        plt.legend()
         plt.figure()
-        plt.plot([0, 1, 2, 3, 4], std_time)
+        for size in range(len(sudoku_size)):
+            plt.plot([0, 1, 2, 3, 4], std_time[size], label=str(sudoku_size[size]**2) + 'x' + str(sudoku_size[size]**2))
         plt.xlabel('Decision Strategy')
         plt.xticks([0, 1, 2, 3, 4], ['CSP', 'DFS_STORE_LEGAL_RECURSIVE', 'DFS_STORE_LEGAL', 'DFS_SORTED_RECURSIVE', 'DFS_SORTED'])
         plt.ylabel('Standard deviation')
         plt.title('Standard deviation of time for different decision strategies')
+        plt.legend()
         plt.show()
-        #print("Time: ", times)
         return
     
 if __name__=="__main__":
     statistics = Statistics()
-    statistics.get_perc_numbers_removed([2,3,4], 10)
-    #statistics.compare_CSP_strategies(4, 10)
-    #statistics.compare_DFS_strategies(4, 10)
+    #statistics.get_perc_numbers_removed([2,3,4], 10)
+    statistics.compare_strategies([3,4], 10)
